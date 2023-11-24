96755c411de354bae0fb6a63d97ee202c453cd2a
Johnson Ikechukwu <johnniekay85@gmail.com>
2023-11-23 21:09:26 -0800

    correction

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,4 +5,3 @@ Contains the class definition of a City
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
